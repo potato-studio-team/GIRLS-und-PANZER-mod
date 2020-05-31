1ed436bfbf467ad53643b6f67094b657b3c41aa8
PotatoMani <52829594+PotatoMani@users.noreply.github.com>
2020-05-31 16:48:08 +0800

    Create RegistryHandler.java

---
+++ src/main/java/com/potatostudio/util/RegistryHandler.java
@@ -0,0 +1,17 @@
+package com.potatostudio.util;
+
+import com.potatostudio.items.ModeItems;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+
+public class RegistryHandler {
+    @SubscribeEvent
+    public static void onItemsRegistry(RegistryEvent.Register<Item> event) {
+        event.getRegistry().register(ModeItems.obsidianingot);
+    }
+}
