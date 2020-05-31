4293fb298bfa5df56b28ed3792112accfdbf4a2c
PotatoMani <52829594+PotatoMani@users.noreply.github.com>
2020-05-31 22:34:59 +0800

    Update RegistryHandler.java

--- src/main/java/com/potatostudio/util/RegistryHandler.java
+++ src/main/java/com/potatostudio/util/RegistryHandler.java
@@ -1,6 +1,9 @@
 package com.potatostudio.util;
 
+import com.potatostudio.blocks.ModBlocks;
 import com.potatostudio.items.ModeItems;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,5 +15,12 @@ public class RegistryHandler {
     @SubscribeEvent
     public static void onItemsRegistry(RegistryEvent.Register<Item> event) {
         event.getRegistry().register(ModeItems.Anglerfish);
+        event.getRegistry().register(new BlockItem(ModBlocks.ooaraiBlock, new Item.Properties())
+                .setRegistryName(ModBlocks.ooaraiBlock.getRegistryName())
+        );
+    }
+    @SubscribeEvent
+    public static void onBlockRegistry(RegistryEvent.Register<Block> event) {
+        event.getRegistry().register(ModBlocks.ooaraiBlock);
     }
 }
