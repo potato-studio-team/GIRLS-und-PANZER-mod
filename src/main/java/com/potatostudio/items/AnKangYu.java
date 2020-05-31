88fb94f4f2f7f984c214ebfc3b332209d89c0471
PotatoMani <52829594+PotatoMani@users.noreply.github.com>
2020-05-31 17:39:12 +0800

    Update AnKangYu.java

---
+++ src/main/java/com/potatostudio/items/AnKangYu.java
@@ -0,0 +1,10 @@
+package com.potatostudio.items;
+
+import net.minecraft.item.Item;
+
+public class AnKangYu extends Item {
+    public AnKangYu() {
+        super(new Properties());
+        this.setRegistryName("ankang_yu");
+    }
+}
