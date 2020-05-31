286ad36eed6dfb5af84d41c94bdd08a655cf3b49
PotatoMani <52829594+PotatoMani@users.noreply.github.com>
2020-05-31 16:48:10 +0800

    Create Obsidianingot.java

---
+++ src/main/java/com/potatostudio/items/Obsidianingot.java
@@ -0,0 +1,10 @@
+package com.potatostudio.items;
+
+import net.minecraft.item.Item;
+
+public class Obsidianingot extends Item {
+    public Obsidianingot() {
+        super(new Properties());
+        this.setRegistryName("obsidian_ingot");
+    }
+}
