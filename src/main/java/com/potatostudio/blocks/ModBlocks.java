824738be12befac3ea99708afc85d497488ee801
PotatoMani <52829594+PotatoMani@users.noreply.github.com>
2020-06-01 20:53:22 +0800

    Update ModBlocks.java

--- src/main/java/com/potatostudio/blocks/ModBlocks.java
+++ src/main/java/com/potatostudio/blocks/ModBlocks.java
@@ -1,5 +1,7 @@
 package com.potatostudio.blocks;
 
+import com.potatostudio.blocks.schoolOwnBlock.OoaraiBlock;
+
 public class ModBlocks {
     public static OoaraiBlock ooaraiBlock = new OoaraiBlock();
 }
