47827faa4ce61834b975266db1c8957e31f9090c
PotatoMani <52829594+PotatoMani@users.noreply.github.com>
2020-06-01 21:25:12 +0800

    Update OoaraiBlock.java

--- src/main/java/com/potatostudio/blocks/schoolSignBlock/OoaraiBlock.java
+++ src/main/java/com/potatostudio/blocks/schoolSignBlock/OoaraiBlock.java
@@ -1,17 +1,28 @@
 package com.potatostudio.blocks.schoolSignBlock;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+
 
 public class OoaraiBlock extends Block {
     public OoaraiBlock() {
         super(Properties.create(Material.ROCK)
+//                方块属性
                 .lightValue(15)
                 .hardnessAndResistance(3)
                 .harvestLevel(2)
                 .sound(SoundType.STONE)
         );
-    this.setRegistryName("ooarai_block");
+    this.setRegistryName("ooarai_block");// 命名
+    }
+
+//    方块状态
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(BlockStateProperties.FACING, BlockStateProperties.POWERED);
     }
 }
