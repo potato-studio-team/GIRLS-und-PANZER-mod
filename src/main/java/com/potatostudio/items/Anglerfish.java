e89f29a45106af11d87ee4bbced881a1e084875f
PotatoMani <52829594+PotatoMani@users.noreply.github.com>
2020-06-03 23:15:20 +0800

    Update Anglerfish.java

--- src/main/java/com/potatostudio/items/Anglerfish.java
+++ src/main/java/com/potatostudio/items/Anglerfish.java
@@ -5,7 +5,7 @@ import net.minecraftforge.common.ToolType;
 
 public class Anglerfish extends Item {
     public Anglerfish() {
-        super(new Properties().maxStackSize(1).addToolType(ToolType.PICKAXE,3).maxDamage(100));
+        super(new Properties().maxStackSize(64).addToolType(ToolType.PICKAXE,3).maxDamage(100));
         this.setRegistryName("angler_fish");
     }
 }
