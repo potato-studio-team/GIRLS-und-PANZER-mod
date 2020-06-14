c89d09b5569c3f84d2b4748784c2d58044ab2dcb
PotatoMani <52829594+PotatoMani@users.noreply.github.com>
2020-06-14 22:47:01 +0800

    something finish

---
+++ src/main/java/com/potatostudio/blocks/ModTileEntitiesTypes.java
@@ -0,0 +1,8 @@
+package com.potatostudio.blocks;
+
+import net.minecraft.tileentity.TileEntityType;
+
+public class ModTileEntitiesTypes {
+    public static TileEntityType<?> TankMakerTileEntity =
+            TileEntityType.Builder.create().build(null);
+}
